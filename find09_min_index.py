81a4d00d3404b52833ece489d5b9a4260b317e65
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-11-11 14:08:40 +0900

    Find09

--- find09_min_index.py
+++ find09_min_index.py
@@ -5,5 +5,14 @@ def find_min_index(data):
         data: list of numbers
     returns: index of minimum number in the list
     """
-    return 0
+    idx=0
+    m=data[idx]
+
+    for i in range(len(data)):
+        if data[i]<m:
+            m=data[i]
+            idx=i
+    return idx
+
+print(find_min_index([5,7,1,15,6,4]))
 
