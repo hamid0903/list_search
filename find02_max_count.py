f0710a95e239bcbe953ca75d3296232c5043f3d5
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-11-11 13:59:01 +0900

    Find02

--- find02_max_count.py
+++ find02_max_count.py
@@ -1,3 +1,6 @@
+from find01_max import find_max
+
+
 def find_max_count(data):
     """
     Given the list of numbers, Find count of maximum numbers in the list
@@ -5,4 +8,15 @@ def find_max_count(data):
         data: list of numbers
     returns: count of maximum numbers in the list
     """
-    return 0
+    n=data[0]
+    
+    for i in data:
+        if i>n:
+            n=i
+    k=0
+    for i in data:
+        if n==i:
+            k+=1    
+    return k
+
+print(find_max_count([5,15,6,15,7,9]))
