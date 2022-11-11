cc0245787dfb756bc4e43e352a446ae80af31fca
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-11-11 14:09:04 +0900

    Find10

--- find10_min_even.py
+++ find10_min_even.py
@@ -5,5 +5,12 @@ def find_min_even(data):
         data: list of numbers
     returns: minimum even number in the list
     """
-    return 0
+    n=data[0]
+
+    for i in data:
+        if i%2==0:
+            if i<n:
+                n=i
+    return n
+print(find_min_even([5,9,10,12]))
 
