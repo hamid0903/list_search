7beca75118fe7138053b5cda3cf10becc65f3e5f
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-11-11 14:05:28 +0900

    Find04

--- find04_max_index.py
+++ find04_max_index.py
@@ -5,4 +5,13 @@ def find_max_index(data):
         data: list of numbers
     returns: index of maximum number in the list
     """
-    return 0
+    idx=0
+    m=data[idx]
+
+    for i in range(len(data)):
+        if data[i]>m:
+            m=data[i]
+            idx=i
+    return idx
+
+print(find_max_index([5,7,1,15,6,4]))
